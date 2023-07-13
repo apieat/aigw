a04aca9f687dbb0634765709a738dd0e0dc2b533
Liu Ming <extraf@live.cn>
2023-07-13 10:42:59 +0800

    add call helper function

--- request.go
+++ request.go
@@ -1,7 +1,11 @@
 package aigw
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/sirupsen/logrus"
 )
@@ -33,4 +37,51 @@ func (c *CompletionRequest) ToPrompt(templates ...map[string]string) string {
 		}
 	}
 	return c.Prompt
+}
+
+//Call calls the completion endpoint
+func (c *CompletionRequest) Call(url string, funcs ...AllowedFunction) error {
+	var bts bytes.Buffer
+	c.Functions = []AllowedFunction{
+		{
+			Path:   "/api/project/fill",
+			Method: "POST",
+		},
+	}
+	err := json.NewEncoder(&bts).Encode(&c)
+	if err == nil {
+		var req *http.Request
+		req, err = http.NewRequest(http.MethodPost, url, &bts)
+		req.Header.Set("Content-Type", "application/json")
+		if err == nil {
+			var resp *http.Response
+			resp, err = http.DefaultClient.Do(req)
+			if err == nil {
+				var bts []byte
+				bts, err = io.ReadAll(resp.Body)
+				var r Response
+				if err == nil {
+					err = json.Unmarshal(bts, &r)
+					if err == nil && r.Success {
+						return err
+					} else if err == nil {
+						return &r
+					}
+				}
+			}
+		}
+	}
+	return err
+}
+
+type Response struct {
+	Err     string `json:"error"`
+	Success bool   `json:"success"`
+}
+
+func (r *Response) Error() string {
+	if r.Success {
+		return ""
+	}
+	return r.Err
 }
